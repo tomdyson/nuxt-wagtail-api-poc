26687a1d9966e8373f49bb726a8466e4b795d585
Tom Dyson <tom@torchbox.com>
2020-06-22 15:38:01 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container pl-16 pt-12 max-w-2xl">
+    <h1 class="text-6xl font-black pb-4">JPL News</h1>
+    <p v-if="$fetchState.pending">Fetching posts...</p>
+    <p v-else-if="$fetchState.error">Error while fetching posts: {{ $fetchState.error.message }}</p>
+    <ul v-else>
+      <li v-for="post of posts.items" :key="post.id" class="pb-4">
+        <n-link :to="`/posts/${post.id}`" class="text-2xl font-bold leading-tight">{{ post.title }}</n-link>
+        <p class="pt-1">{{ post.introduction }}</p>
+        <p class="pt-1 pb-4 text-gray-600">{{ post.publication_date|datify }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: []
+    };
+  },
+  head() {
+    return {
+      title: "JPL News",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "News from NASA / JPL"
+        }
+      ]
+    };
+  },
+  async fetch() {
+    this.posts = await this.$http.$get(
+      "https://jpl-poc.torchbox.com/api/v2/pages/?type=news.NewsPage&fields=introduction,body,publication_date"
+    );
+  },
+  filters: {
+    datify: function(value) {
+      if (value && value.length) {
+        var d = new Date(Date.parse(value));
+        var options = { year: "numeric", month: "long", day: "numeric" };
+        return d.toLocaleDateString("en-us", options);
+      }
+    }
+  }
+};
+</script>
---
+++ pages/posts/_id.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container pl-16 pt-12 max-w-2xl">
+    <div v-if="$fetchState.pending">Fetching post #{{$route.params.id}}...</div>
+    <div v-else>
+      <h1 class="text-4xl font-black pb-4 leading-tight">{{ item.title }}</h1>
+      <p>
+        <n-link to="/" class="underline">Home</n-link>
+      </p>
+      <img
+        v-if="item.hero_thumbnail"
+        :src="'' + item.hero_thumbnail.url"
+        :width="item.hero_thumbnail.width"
+        :height="item.hero_thumbnail.height"
+        class="pt-4 pb-4 rounded"
+      />
+      <h3 class="text-2xl pb-4 leading-tight">{{ item.introduction }}</h3>
+      <span v-for="block in item.body" :key="block.id" id="streamfield">
+        <div v-if="block.type == 'heading'">
+          <h2 class="text-xl pb-4 leading-tight">{{ block.value }}</h2>
+        </div>
+
+        <div v-else-if="block.type == 'paragraph'">
+          <p v-html="block.value"></p>
+        </div>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      item: {}
+    };
+  },
+  head() {
+    return {
+      title: this.item.title,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.item.introduction
+        }
+      ]
+    };
+  },
+  async fetch() {
+    this.item = await this.$http.$get(
+      `https://jpl-poc.torchbox.com/api/v2/pages/${this.$route.params.id}/`
+    );
+  }
+};
+</script>
+
+<style>
+#streamfield p {
+  @apply mb-4;
+}
+</style>
